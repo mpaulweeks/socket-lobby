ba902d5332d5bb3a13502da483d28acfef5be9af
mpaulweeks <mpaulweeks@gmail.com>
2018-02-06 15:56:06 -0500

    test message ctors

--- lobby-server/message_test.go
+++ lobby-server/message_test.go
@@ -1,14 +1,47 @@
 package main
 
-import "testing"
+import (
+	"encoding/json"
+	"strconv"
+	"testing"
+)
 
-func TestLobbyRefreshMessage(t *testing.T) {
-	client := Client{
-		app:   "a",
-		lobby: "b",
-		id:    "c",
+var (
+	testCounter = 0
+)
+
+func ToJson(data interface{}) string {
+	jsonBytes, err := json.Marshal(data)
+	if err != nil {
+		return ""
 	}
-	sut := newLobbyRefreshMessage(&client)
+	return string(jsonBytes)
+}
+
+func StrToBytes(in string) []byte {
+	return []byte(in)
+}
+
+func NewTestString() string {
+	testCounter += 1
+	return "testString-" + strconv.Itoa(testCounter)
+}
+
+func newTestClient() *Client {
+	client := Client{
+		app:   NewTestString(),
+		lobby: NewTestString(),
+		id:    NewTestString(),
+	}
+	return &client
+}
+
+func TestRegisterMessage(t *testing.T) {
+	client := newTestClient()
+	sut := newRegisterMessage(client)
+	if MessageTypeRegister != sut.Type {
+		t.Error("Type incorrectly set")
+	}
 	if client.app != sut.App {
 		t.Error("App incorrectly set")
 	}
@@ -17,5 +50,52 @@ func TestLobbyRefreshMessage(t *testing.T) {
 	}
 	if client.id != sut.ClientID {
 		t.Error("ClientID incorrectly set")
+	}
+}
+
+func TestMessage(t *testing.T) {
+	rawData := map[string]string{
+		"type":      NewTestString(),
+		"client_id": NewTestString(),
+		"app":       NewTestString(),
+		"lobby":     NewTestString(),
+		"message":   NewTestString(),
+	}
+	rawJson := StrToBytes(ToJson(rawData))
+	sut := newMessage(rawJson)
+	if rawData["type"] != sut.Type {
+		t.Error("Type incorrectly set")
+	}
+	if rawData["app"] != sut.App {
+		t.Error("App incorrectly set")
+	}
+	if rawData["lobby"] != sut.Lobby {
+		t.Error("Lobby incorrectly set")
+	}
+	if rawData["client_id"] != sut.ClientID {
+		t.Error("ClientID incorrectly set")
+	}
+	if rawData["message"] != sut.Message {
+		t.Error("Message incorrectly set")
+	}
+}
+
+func TestLobbyRefreshMessage(t *testing.T) {
+	client := newTestClient()
+	sut := newLobbyRefreshMessage(client)
+	if MessageTypeLobbyRefresh != sut.Type {
+		t.Error("Type incorrectly set")
+	}
+	if client.app != sut.App {
+		t.Error("App incorrectly set")
+	}
+	if client.lobby != sut.Lobby {
+		t.Error("Lobby incorrectly set")
+	}
+	if client.id != sut.ClientID {
+		t.Error("ClientID incorrectly set")
+	}
+	if "{}" != sut.Message {
+		t.Error("Message incorrectly set")
 	}
 }
