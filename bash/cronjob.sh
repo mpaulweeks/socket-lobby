79817c9b10a11b340116ebdca63de3286ba962f6
mpaulweeks <mpaulweeks@gmail.com>
2018-02-09 03:50:52 -0500

    try new cronjob with redundant commit check

--- bash/cronjob.sh
+++ bash/cronjob.sh
@@ -1,10 +1,12 @@
 # * * * * * ec2-user cd /home/ec2-user/socket-lobby && ./bash/cronjob.sh
 
+oldCommit=`cat lobby-server/tmp/git.log`
 git pull
 git rev-parse HEAD > lobby-server/tmp/git.log
-status=`curl -s -o /dev/null -w "%{http_code}" localhost:5110/api/git`
-if ! [[ $status == "200" ]]
+newCommit=`cat lobby-server/tmp/git.log`
+if ! [[ $oldCommmit == $newCommit ]]
 then
+  curl http://localhost:5110/api/git
   sleep 2
   ./bash/bg_socket.sh
 fi
